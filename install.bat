cdff5a5bda3fca34d5a5887de1301fd6630131e8
arroyoquiel <81461845+arroyoquiel@users.noreply.github.com>
2023-04-07 05:05:27 -0600

    Fancier [Y/N]
    Fancier [Y/N], does not require for you to press the letter and then enter, and also asks you if you want to download the model instead of downloading it automatically if you don't have it.
    
    Also lets you retry the download if failed or disconnected.

--- install.bat
+++ install.bat
@@ -130,22 +130,39 @@ IF NOT EXIST models (
 )
 
 IF NOT EXIST models/gpt4all-lora-quantized-ggml.bin (
-    powershell -Command "Invoke-WebRequest -Uri 'https://the-eye.eu/public/AI/models/nomic-ai/gpt4all/gpt4all-lora-quantized-ggml.bin' -OutFile 'models/gpt4all-lora-quantized-ggml.bin'"
-) ELSE (
-    :PROMPT
+    :PROMPT_DOWNLOAD
     echo.
-    set /p choice="The model file already exists. Do you want to override it? (y/n) "
-    if /i {%choice%}=={y} (
-        powershell -Command "Invoke-WebRequest -Uri 'https://the-eye.eu/public/AI/models/nomic-ai/gpt4all/gpt4all-lora-quantized-ggml.bin' -OutFile 'models/gpt4all-lora-quantized-ggml.bin'" && goto :CONTINUE
-    ) else if /i {%choice%}=={n} (
-        goto :CONTINUE
-    ) else (
-        echo.
-        echo Invalid input. Please enter 'y' or 'n'.
-        goto :PROMPT
-    )
+    choice /C YN /M "The default model file (gpt4all-lora-quantized-ggml.bin) does not exist. Do you want to download it?"
+    if errorlevel 2 goto DOWNLOAD_SKIP
+    if errorlevel 1 goto MODEL_DOWNLOAD
+) ELSE (
+    :PROMPT_OVERRIDE
+    echo.
+    choice /C YN /M "The default model file (gpt4all-lora-quantized-ggml.bin) already exists. Do you want to override it?"
+    if errorlevel 2 goto DOWNLOAD_SKIP
+    if errorlevel 1 goto MODEL_DOWNLOAD
 )
+:MODEL_DOWNLOAD
+echo.
+echo Downloading latest model...
+powershell -Command "Invoke-WebRequest -Uri 'https://the-eye.eu/public/AI/models/nomic-ai/gpt4all/gpt4all-lora-quantized-ggml.bin' -OutFile 'models/gpt4all-lora-quantized-ggml.bin'"
+if errorlevel 1 (
+    echo Failed to download model. Please check your internet connection.
+    choice /C YN /M "Do you want to try downloading again?"
+    if errorlevel 2 goto DOWNLOAD_SKIP
+    if errorlevel 1 goto MODEL_DOWNLOAD
+) else (
+    echo Model successfully downloaded.
+)
+goto :CONTINUE
+
+:DOWNLOAD_SKIP
+echo.
+echo Skipping download of model file...
+goto :CONTINUE
+
 :CONTINUE
+echo.
 
 echo Cleaning tmp folder
 rd /s /q "./tmp"
